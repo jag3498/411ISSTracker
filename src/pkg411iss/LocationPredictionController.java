6ff61ff0be24fedb53ba32cd122dd9e9581a453a
Joey Queppet <Joeq747@gmail.com>
2019-01-30 20:23:35 -0500

    Processed the parsing.
     Prints an error if the user puts in bad data. Allows it to continue otherwise.

--- src/pkg411iss/LocationPredictionController.java
+++ src/pkg411iss/LocationPredictionController.java
@@ -8,6 +8,7 @@ package pkg411iss;
 import com.sun.deploy.util.StringUtils;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +16,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.text.SimpleDateFormat;  
 import org.json.simple.parser.ParseException;
 
 /**
@@ -53,14 +55,24 @@ public class LocationPredictionController implements Initializable {
     public void lookUpTime(){
         ISSProvider iss = new ISSProvider();
         try{
-            if(StringContainsOnlyNumbers(latitude.getText()) &&
-                    StringContainsOnlyNumbers(longitude.getText()))
+            
+            if(StringContainsOnlyNumbers(latitude.getText()) && StringContainsOnlyNumbers(longitude.getText()) )
             {
-                String lat = latitude.getText();
-                String lon = longitude.getText();
-                RiseTime riseTime = iss.nextPass(lat,lon);
-        
-        System.out.println("Rise Time" + riseTime.getDateTime());
+                double lat = Double.parseDouble(latitude.getText());
+                double lon = Double.parseDouble(longitude.getText());
+                if(lat >= -90 && lat <=90 && lat!=0 && lon >= -90 && lon <=90 && lon!=0){
+                    //check for the next passing of the iss, and set this value into a riseTime obj
+                   RiseTime riseTime = iss.nextPass(latitude.getText(),longitude.getText());
+                   //format thegetDateTime() method's returned date into a string
+                   DateFormat dateFormat = new SimpleDateFormat("yyy-mm-dd hh:mm:ss");
+                   String strRiseTime = dateFormat.format(riseTime.getDateTime());
+                   
+                   responseField.setText("The next pass will be: " + strRiseTime);
+                    System.out.println("Rise Time" + riseTime.getDateTime()); 
+                }else{
+                    responseField.setText("Error, please use a valid latitude and longitude. \n Proper values fall between 90 and -90, excluding 0");
+                }
+                
             }else { 
                 responseField.setText("Error, please use a valid latitude and longitude. \n Proper values fall between 90 and -90, excluding 0");
             }
